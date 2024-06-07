bda131265d498e9ae5823e90376e10a1d3475723
DIEGO_SEBASTIAN_LARA_BAEZA <a.21308051280926@cbtis128.edu.mx>
2024-06-07 15:16:39 +0000

    Actualizado

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:lara/pages/list_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Widget> _pages = [
+    const PersonList(),
+  ];
+
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Flutter Firebase',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color(0xffbeb97c),
+      ),
+      body: _pages[_selectedIndex],
+    );
+  }
+}
